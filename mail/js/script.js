5c46c71ad00c6bac66690c1094c8f6947c21c4d6
SalvatoreVillano <villano.salvatore9@gmail.com>
2022-10-11 17:13:56 +0200

    mail working ok

---
+++ mail/js/script.js
@@ -0,0 +1,24 @@
+const emails = ['admin@gmail.com', 'user@gmail.com', 'mod@gmail.com'];
+let cond = true;
+
+document.getElementById('esegui').addEventListener('click', function(){
+
+    for(let i = 0;i<emails.length; i++){
+    
+    const userEmail = document.getElementById('email').value
+    console.log( userEmail +" == "+emails[i])
+    if (userEmail == emails[i] || cond == true){
+        document.getElementById('esegui').classList.add('d-none')
+        document.getElementById('messageBox').classList.add('d-block')
+        document.getElementById('messageBox').innerHTML = '<p>sei presente nella lista degli invitati<p>'
+        cond = false;
+    } else {
+        document.getElementById('esegui').classList.add('d-none')
+        document.getElementById('messageBox').classList.add('d-block')
+        document.getElementById('messageBox').innerHTML = '<p>mi dispiace, non sei presente nella lista degli invitati<p>'
+    }
+    }
+})
+
+
+
